3d8b2845f770979c7c0d3260a35f871d1a259ab7
TairBusi <tairbosa@gmail.com>
2021-04-02 12:22:32 +0300

    mat22

---
+++ text.py
@@ -0,0 +1,29 @@
+def revword(word:str) ->str:
+    return word[::-1].lower()
+
+def countword() ->int:
+    count=0
+    i=0
+    fhand=open('text.txt','r')
+    readfile= fhand.read()
+    word1= readfile.split()[0] #אולי צריך להוסיף פה lower
+    numofwords= len(readfile.split())
+    while i <= numofwords:
+        if word1 == revword(readfile[i-1]):
+            count=count+1
+            i=i+1
+    print(count)
+
+    
+        
+    
+    
+    
+    
+    
+    
+    
+
+
+
+        
